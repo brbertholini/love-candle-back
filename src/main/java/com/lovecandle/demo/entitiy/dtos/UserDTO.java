1419e2c3fcb07d269953ec482b825c48caab12f7
Giovanni Ultramari <giovanniultramari@gmail.com>
2024-03-27 10:37:28 -0300

    FEAT: create UserDTO
    Creating Data Transfer Object to User

---
+++ src/main/java/com/lovecandle/demo/entitiy/dtos/UserDTO.java
@@ -0,0 +1,24 @@
+package com.lovecandle.demo.entitiy.dtos;
+
+import com.lovecandle.demo.entitiy.User;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.phone = user.getPhone();
+        this.password = user.getPassword();
+    }
+}
